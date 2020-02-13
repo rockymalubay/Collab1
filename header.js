7c82ff0be9670e0b83b84edd7da716cec7e0f879
rockymalubay <rockymalubay@gmail.com>
2020-02-13 15:48:23 -0800

    fixed footer

--- header.js
+++ header.js
@@ -1,8 +1,8 @@
 
-function Select(){
-    document.querySelector(".menu").style.borderBottom = "2px solid white";
-}
-function SelectOff(){
-    document.querySelector(".menu").style.borderBottom = "none";
-}
+// function Select(){
+//     document.querySelector(".menu").style.borderBottom = "2px solid white";
+// }
+// function SelectOff(){
+//     document.querySelector(".menu").style.borderBottom = "none";
+// }
 
